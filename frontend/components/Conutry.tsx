ad0bd03ab9b25355230cc313ccf2874a61cc2728
Your Name <your_email@example.com>
2024-08-08 20:32:55 +0530

    News

---
+++ frontend/components/Conutry.tsx
@@ -0,0 +1,8 @@
+export const countries = [
+    "India",  "United States", "Canada", "United Kingdom", "Australia", "Germany", "France",
+    "Japan", "China", "Brazil", "Russia", "Italy", "Spain", "Mexico", "South Korea", "South Africa", "New Zealand",
+    "Argentina", "Saudi Arabia", "Sweden", "Switzerland", "Netherlands", "Norway", "Finland",
+    "Denmark", "Belgium", "Austria", "Poland", "Portugal", "Greece", "Turkey", "Israel", "United Arab Emirates", "Singapore",
+    "Malaysia", "Indonesia", "Thailand", "Vietnam", "Philippines", "Egypt", "Nigeria", "Kenya", "Chile",
+    "Peru", "Colombia", "Venezuela", "Iran", "Pakistan", "Bangladesh", "Qatar"
+  ];
