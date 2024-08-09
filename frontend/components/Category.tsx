de9a8e410f186980683b2a22dad2071b988720fc
Your Name <your_email@example.com>
2024-08-09 17:12:12 +0530

    complete categories

--- frontend/components/Category.tsx
+++ frontend/components/Category.tsx
@@ -1,4 +1,5 @@
 export const categories = [
+  
   'National News', 'International News','Politics', 'Business', 'Technology',
   'Sports', 'Entertainment', 'Health', 'Science', 'Education', 'Lifestyle',
   'Environment', 'Crime', 'Opinion', 'Regional News', 'Economy', 'World Events',
